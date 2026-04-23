6e1ff4d8de0c6790f048b1e30b686ae91124171b
teddosan <teddo3@gmail.com>
2026-04-23 17:08:53 -0400

    Connecting to local db. Need to seed user / session

--- utils/db.ts
+++ utils/db.ts
@@ -1,4 +1,15 @@
+// utils/db.ts
 import { Pool } from "npm:pg";
 
-// Make sure 'export' is right here!
-export const pool = new Pool();
+const connectionString = Deno.env.get("DATABASE_URL");
+
+export const pool = new Pool({
+  // CORRECT: Wrap the string in an object
+  connectionString: connectionString,
+
+  // Also, since you are connecting from WSL to Windows,
+  // you might need this to avoid SSL issues locally
+  ssl: connectionString?.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
+});
